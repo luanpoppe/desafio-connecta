118d0aa1d14a7db33282b57aad2d432e01b55361
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2026-04-25 14:34:13 -0300

    feat: integrate Prisma with backend, add database configuration and update dependencies

--- packages/backend/eslint.config.mjs
+++ packages/backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'jest.config.js'],
+    ignores: ['eslint.config.mjs', 'jest.config.js', 'src/generated/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
