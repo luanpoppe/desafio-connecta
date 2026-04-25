c5de0346d80f3d10422b0e0f078c5cdd0eb5d5ab
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2026-04-25 11:18:52 -0300

    feat: add UsersModule and CartsModule to the application

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { CoreModule } from './core/core.module';
+import { UsersModule } from './modules/users/users.module';
+import { CartsModule } from './modules/carts/carts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CoreModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CoreModule,
+    UsersModule,
+    CartsModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
